6a48aa3f34a84c933e9689fff2b4e68f268dbca9
nick3dddsss-ctrl <nick3dddsss@gmail.com>
2026-09-24 00:10:36 +0400

    Bump CineDeck Online to 1.1.4

--- CineDeckTV/app/build.gradle.kts
+++ CineDeckTV/app/build.gradle.kts
@@ -2,6 +2,6 @@ plugins { id("com.android.application") }
 android {
  namespace = "com.cinedeck.tv"
  compileSdk = 36
- defaultConfig { applicationId = "com.cinedeck.tv"; minSdk = 21; targetSdk = 28; versionCode = 14; versionName = "1.1.3-online" }
+ defaultConfig { applicationId = "com.cinedeck.tv"; minSdk = 21; targetSdk = 28; versionCode = 15; versionName = "1.1.4-online" }
  compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
 }
