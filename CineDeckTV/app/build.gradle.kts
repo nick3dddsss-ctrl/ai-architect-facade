457f5f5019fd984e06a72fd6d35f835997943996
nick3dddsss-ctrl <nick3dddsss@gmail.com>
2026-09-23 21:14:17 +0400

    Bump CineDeck version to 0.5.0

--- CineDeckTV/app/build.gradle.kts
+++ CineDeckTV/app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.cinedeck.tv"
         minSdk = 23
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.4.0"
+        versionCode = 5
+        versionName = "0.5.0"
     }
 
     compileOptions {
