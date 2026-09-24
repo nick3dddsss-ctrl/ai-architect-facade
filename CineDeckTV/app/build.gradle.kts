79d9e1881ff365fa1811ca16c644dc42c0d74cc4
nick3dddsss-ctrl <nick3dddsss@gmail.com>
2026-09-25 02:10:26 +0400

    Bump CineDeck aggregator build to 1.4.0

--- CineDeckTV/app/build.gradle.kts
+++ CineDeckTV/app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins { id("com.android.application") }
 android {
  namespace = "com.cinedeck.tv"
  compileSdk = 36
- defaultConfig { applicationId = "com.cinedeck.tv"; minSdk = 21; targetSdk = 28; versionCode = 19; versionName = "1.3.0-auto" }
+ defaultConfig { applicationId = "com.cinedeck.tv"; minSdk = 21; targetSdk = 28; versionCode = 20; versionName = "1.4.0-aggregator" }
  compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
 }
 dependencies {
