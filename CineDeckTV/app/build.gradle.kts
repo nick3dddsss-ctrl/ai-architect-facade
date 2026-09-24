ad9298548ec3be4f4cfc6279e83fcc2820ed83d2
nick3dddsss-ctrl <nick3dddsss@gmail.com>
2026-09-25 02:26:49 +0400

    Bump CineDeck to 1.5 provider engine

--- CineDeckTV/app/build.gradle.kts
+++ CineDeckTV/app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins { id("com.android.application") }
 android {
  namespace = "com.cinedeck.tv"
  compileSdk = 36
- defaultConfig { applicationId = "com.cinedeck.tv"; minSdk = 21; targetSdk = 28; versionCode = 20; versionName = "1.4.0-aggregator" }
+ defaultConfig { applicationId = "com.cinedeck.tv"; minSdk = 21; targetSdk = 28; versionCode = 21; versionName = "1.5.0-provider" }
  compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
 }
 dependencies {
