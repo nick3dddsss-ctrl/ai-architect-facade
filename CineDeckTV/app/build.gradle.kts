a64777c4dba53213f9a65e07092329a61a2fde09
nick3dddsss-ctrl <nick3dddsss@gmail.com>
2026-09-24 21:12:52 +0400

    Bump CineDeck stable to 1.1.6

--- CineDeckTV/app/build.gradle.kts
+++ CineDeckTV/app/build.gradle.kts
@@ -2,6 +2,6 @@ plugins { id("com.android.application") }
 android {
  namespace = "com.cinedeck.tv"
  compileSdk = 36
- defaultConfig { applicationId = "com.cinedeck.tv"; minSdk = 21; targetSdk = 28; versionCode = 16; versionName = "1.1.5-stable" }
+ defaultConfig { applicationId = "com.cinedeck.tv"; minSdk = 21; targetSdk = 28; versionCode = 17; versionName = "1.1.6-stable" }
  compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
 }
